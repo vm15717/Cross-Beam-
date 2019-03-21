74ad3272808c7a6eeb0baa6ea369a25dbee328a4
Rishindra <vm15717@my.bristol.ac.uk>
2019-03-21 15:37:02 +0000

    changed initialstructmode1

--- InitialStruct1mode.m
+++ InitialStruct1mode.m
@@ -1,17 +1,17 @@
-tic
-%Run freq. analysis of case, built into Freq_Analysis.py
-dos('abaqus cae noGUI=TorsMerging.py');
-toc
-[Phi,Lam,N,diagKs]=Get_Modal_v2;
+% tic
+% %Run freq. analysis of case, built into Freq_Analysis.py
+% dos('abaqus cae noGUI=TorsMerging.py');
+% toc
+% [Phi,Lam,N,diagKs]=Get_Modal_v2;
 % Run matlab file to convert mass and stiff matrices to get modal forces as
 % .csv file to plug back into next abaqus file
-% load('matrices2.mat');
+ load('matrices2.mat');
 p=0;
 af=linspace(-1,1,10);
 excite=[1];
 nm=0;
 sf=(1e-1:1e-1:1e+0)*10;
-for k= 1:length(sf)
+for k= 2:length(sf)
     for i = 1:length(af)
         p=p+1;
         forcecalc(Phi,Lam,N,diagKs,af(i)*sf(k),excite);
